fbce1043f773b90ded746ae9e8bc2cd03fbd558b
hossein.naseri <hossein.naseri.info@gmail.com>
2024-08-01 15:40:16 +0330

    fix(root): delete alias config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { Examination } from '@Entities';
+import { Examination } from './models';
 
 @Module({
   imports: [
