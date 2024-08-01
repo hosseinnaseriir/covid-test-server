9fab4e8bd158cad2287ba2085d60779a76a4b8bb
hossein.naseri <hossein.naseri.info@gmail.com>
2024-08-01 16:16:36 +0330

    feat(modules): implement authentication module

---
+++ src/modules/authentication/auth.controller.ts
@@ -0,0 +1,40 @@
+import {
+    Body, Controller, HttpCode, HttpException, HttpStatus, Post, UseGuards
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
+import { LoginPayload, RegisterPayload } from 'src/models';
+import { Public } from 'src/decorators';
+
+
+@Controller('auth')
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @Public()
+    @Post("/register")
+    @HttpCode(HttpStatus.CREATED)
+    async register(@Body() registerPayload: RegisterPayload) {
+        try {
+            const user = await this.authService.registerUser(registerPayload);
+            return { message: 'Registration Successful', user };
+        } catch (ex) {
+            throw new HttpException(ex.message, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @Public()
+    @Post("/validate")
+    @UseGuards(AuthGuard('local'))
+    @HttpCode(HttpStatus.OK)
+    async validate(@Body() loginPayload: LoginPayload) {
+        try {
+            const user = await this.authService.validateUser(loginPayload);
+            const token = await this.authService.generateJwtToken(user);
+            return { message: "Login Seccessfull", user, token }
+        } catch (ex) {
+            throw new HttpException(ex.message, HttpStatus.UNAUTHORIZED);
+        }
+    }
+
+}
---
+++ src/modules/authentication/auth.module.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { LocalStrategy } from './local.strategy';
+import { UserJwtGuard } from 'src/guards';
+import { PassportModule } from '@nestjs/passport';
+import { User } from 'src/models';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User]),
+  ConfigModule.forRoot(),
+    PassportModule,
+  JwtModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: async (_configService: ConfigService) => ({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+    }),
+    inject: [ConfigService],
+  }),
+  ],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    LocalStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: UserJwtGuard,
+    },
+  ],
+  controllers: [AuthController],
+  exports: [JwtModule, AuthService],
+})
+export class AuthModule { }
