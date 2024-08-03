bb92a07bc97582637c58beb2c7b671766b18a85b
hossein.naseri <hossein.naseri.info@gmail.com>
2024-08-03 12:32:28 +0330

    feat(modules/examinations): implement examinations and location and stats api

--- src/modules/examinations/examinations.controller.ts
+++ src/modules/examinations/examinations.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Query, Version } from '@nestjs/common';
-import { Public } from 'src/decorators';
+import { Controller, Get, Param, Query, Version } from '@nestjs/common';
+import { Public } from '../../decorators';
 import { ExaminationsService } from './examinations.service';
 
 @Controller('examinations')
@@ -15,4 +15,30 @@ export class ExaminationsController {
     ) {
         return this.examinationsService.getExaminationsList(page, limit)
     }
+
+    @Public()
+    @Get('stats')
+    @Version('1')
+    async getExaminationsStats() {
+        return await this.examinationsService.getExaminationsStats();
+    }
+
+    @Public()
+    @Get('/:locationId')
+    @Version('1')
+    async getExaminationsByLocationId(
+        @Param('locationId') locationId: number,
+        @Query('page') page: number = 1,
+        @Query('limit') limit: number = 10
+    ) {
+        return await this.examinationsService.getExaminationsByLocationId(locationId, page, limit);
+    }
+
+    @Public()
+    @Get('locations')
+    @Version('1')
+    getLocationIds() {
+        return this.examinationsService.getLocationIds();
+    }
+
 }
--- src/modules/examinations/examinations.service.ts
+++ src/modules/examinations/examinations.service.ts
@@ -33,4 +33,54 @@ export class ExaminationsService {
         };
     }
 
+    async getExaminationsByLocationId(locationId: number, page: number, limit: number) {
+        if (page < 1 || limit < 1) {
+            throw new HttpException('Page and limit must be positive integers', HttpStatus.BAD_REQUEST);
+        }
+
+        const [result, total] = await this.examinationRepository.findAndCount({
+            where: { locationId },
+            take: limit,
+            skip: (page - 1) * limit,
+        });
+
+        if (total === 0) {
+            throw new HttpException('No examinations found for the given location ID!', HttpStatus.NO_CONTENT);
+        }
+
+        return {
+            data: result,
+            total,
+            page,
+            limit,
+        };
+    }
+
+    async getLocationIds(): Promise<number[]> {
+        const locations = await this.examinationRepository
+            .createQueryBuilder('examination')
+            .select('DISTINCT examination.locationId')
+            .orderBy('examination.locationId', 'ASC')
+            .getRawMany();
+
+        return locations.map(location => location.locationId);
+    }
+
+    async getExaminationsStats() {
+        const rawData = await this.examinationRepository
+            .createQueryBuilder('examination')
+            .select('examination.locationId')
+            .addSelect('SUM(CASE WHEN examination.result = \'pending\' THEN 1 ELSE 0 END)', 'pending')
+            .addSelect('SUM(CASE WHEN examination.result = \'negative\' THEN 1 ELSE 0 END)', 'negative')
+            .addSelect('SUM(CASE WHEN examination.result = \'positive\' THEN 1 ELSE 0 END)', 'positive')
+            .groupBy('examination.locationId')
+            .getRawMany();
+
+        return rawData.map(data => ({
+            locationId: Number(data.examination_locationId),
+            pending: Number(data.pending),
+            negative: Number(data.negative),
+            positive: Number(data.positive),
+        }));
+    }
 }
